0c865d0977b7bf734fb9ea696795e43d351566d2
Charles Silva <c.batista.dos.santos@avanade.com>
2018-06-17 12:50:33 -0300

    create left section

--- src/app/containers/left/left.component.ts
+++ src/app/containers/left/left.component.ts
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left.component.scss']
 })
 export class LeftComponent implements OnInit {
+  public menu = [
+    {icon: "language", text: "Painel de Gestão"},
+    {icon: "fingerprint", text: "Conta Digital"},
+    {icon: "sync", text: "Ant. de Recebíveis"}
+  ]
 
   constructor() { }
 
